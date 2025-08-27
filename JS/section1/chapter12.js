0b0cd404046e03b4bbe66dbab0ab9898e3c82eab
Hyun731 <gilhyun.gwon0731@gmail.com>
2025-08-27 12:24:09 +0900

    1일차 JS 기초

--- JS/section1/chapter12.js
+++ JS/section1/chapter12.js
@@ -1,3 +1,4 @@
+//함수 표현식
 function funcA(){
     console.log("funcA");
 }
@@ -5,7 +6,7 @@ function funcA(){
 let varA = funcA;
 varA()
 
-let varB = function funcB(){
+let varB = function funcB(){ 
     console.log("funcB");
 };
 
@@ -13,6 +14,14 @@ let varB = function funcB(){
 varB() //가능
 // funcB() -> 이건 불가능 그래서 아래처럼 익명 함수(아래 함수를 지칭하는 이름)로 만듦
 
-varB = function (){
+varB = function (){ //이런식으로 값으로 넣은 함수는 데이터로 보기 때문이 호이스팅이 안됨
     console.log("funcB");
 };
+//화살표 함수
+let varC = () => {
+    return 1;
+}
+varC = () => 1; //위랑 똑같음 이렇게도 표현이 가능하다.
+console.log(varC())
+varC = (value) => value + 1
+console.log(varC())
